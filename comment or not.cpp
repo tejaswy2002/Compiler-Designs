0de19d9259762cd238a4591733732ca6b9534db1
tejaswy2002 <113495301+tejaswy2002@users.noreply.github.com>
2022-09-19 20:24:07 +0530

    Create comment or not.cpp

---
+++ comment or not.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+char com [30];
+int i=2,a=0;
+printf("\n Enter Text : ");
+gets(com);
+if(com[0]=='/')
+{
+if(com[1]=='/')
+printf("\n It is a single line Comment.");
+else if(com [1]=='*')
+{
+for(i=2;i<=30;i++)
+{
+if(com[i]=='*'&&com[i+1]=='/')
+{
+printf("\n It is a multi line Comment.");
+a=1;
+break;
+}
+else continue;
+}
+if(a==0)
+printf("\n It is Not a Comment.");
+}
+else
+printf("\n It is Not a Comment.");
+}
+else
+printf("\n It is Not a Comment.");
+}
+OUTPUT:
+ Enter Text : technicalnotes.com
+
+ It is Not a Comment.
+
+ Enter Text : //thetechnicalnotes.com
+
+ It is a single line Comment.
